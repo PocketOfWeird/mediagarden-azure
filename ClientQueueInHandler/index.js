f7cc76126a3443d8e74d8982ee5285a002b402f7
Pocket of Weird <hartzlernathan@gmail.com>
2017-06-07 15:33:03 -0500

    fleshing out ClientInHandler

--- ClientQueueInHandler/index.js
+++ ClientQueueInHandler/index.js
@@ -1,4 +1,41 @@
-module.exports = function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem)
+const jwt = require('jsonwebtoken')
+const { checkRoles, errorAction } = require('../shared/helpers')
+
+
+const sendErrorToClient = message => {
+  context.bindings.clientStateOut = [errorAction(message)]
+  context.done()
+}
+
+const outToQueue = (queue, roleGroup, action) => checkRoles(action, roleGroup,
+  sendErrorToClient('Insufficient permissions'),
+  () => {
+    context.bindings[queue + 'QueueOut'] = [action]
     context.done()
+  }
+)
+
+module.exports = function (context, action) {
+    if (action.type && action.payload && action.meta) {
+      jwt.verify(action.meta.token, process.env['temp_client_key_secret'], (err, user) => {
+        if (!err) {
+
+          action.meta.user = user
+
+          switch (action.type) {
+            case 'SERVER_SCRIPT_POST':
+              outToQueue('scriptPost', 'fasta', action)
+              break;
+            case 'SERVER_AGREEMENT_POST':
+              outToQueue('agreementPost', 'fasta', action)
+            default:
+              sendErrorToClient('Invalid action type')
+          }
+        } else {
+          sendErrorToClient('Invalid token')
+        }
+      })
+    } else {
+      sendErrorToClient('Invalid action')
+    }
 }
--- shared/helpers.js
+++ shared/helpers.js
@@ -1,5 +1,30 @@
+const roleGroups = {
+	all: ['Faculty', 'Staff', 'Student']
+	fasta: ['Faculty', 'Staff'],
+	fa: ['Faculty'],
+	sta: ['Staff'],
+	stu: ['Student']
+}
+
+const checkRoles = (action, roleGroup, errorCb, successCb) => {
+	if (roleGroups[roleGroup].includes(action.meta.user.attributes.primary_role)) {
+		successCb()
+	} else {
+		errorCb()
+	}
+}
+
+const errorAction = message => ({
+	type: 'SERVER_ERROR',
+	payload: new Error(message),
+	error: true
+})
+
 const jsonToBase64Str = json => new Buffer(JSON.stringify(json)).toString("base64")
 
+
 module.exports  = {
+	checkRoles,
+	errorAction,
 	jsonToBase64Str
 }
